5a0c734f5494835710ff3ca492ef3744b6ca8840
Nhlanhla Khumalo <ntg567497@gmail.com>
2025-10-15 19:51:11 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavBar from "./components/NavBar.jsx";
+
+export const App = () => {
+    return (
+        <main>
+            <NavBar/>
+        </main>
+    )
+}
